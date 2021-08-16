250c6c175018bfe7cb1846b63213e28142a194c7
Brittnie <brittniesexton95@gmail.com>
2021-08-16 16:58:38 -0500

    Completed first exercise

---
+++ HelloDungeon/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloDungeon
+{
+    class Game
+    {
+        public void Run()
+        {
+            //
+            /*int healthRegen = 50;
+            int health = 100;
+            health += healthRegen;
+            Console.WriteLine(health);
+
+            string name = "Empty";
+            Console.WriteLine("Please enter your name. ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            */
+
+            string playerName = "Empty";
+            float playerHeath = 100.0f;
+            int level = 1;
+            float enemyHealth = 100.0f;
+            int playerAttackDamage1 = 30;
+            int playerAttackDamage2 = 20;
+            int enemyAttackDamage = 20;
+            int inventory = 5;
+            int lowLevelSwordDamage = 40;
+            int lowLevelPistolDamage = 40;
+
+            Console.WriteLine("What is your adventure name.");
+            playerName = Console.ReadLine();
+            Console.Write("Hello " + playerName);
+            Console.WriteLine(", welcome to your first adventure!");
+
+            //Console.WriteLine("Please choose starting weapon.");
+            // Code to display weapon choices
+            // Code for users weapon selection
+        }
+    }
+}
