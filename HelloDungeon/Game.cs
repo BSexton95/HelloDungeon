2c43015d6384c464fbbb894b113f7d5db14ee3ac
Brittnie <brittniesexton95@gmail.com>
2021-08-23 15:02:21 -0500

    Cleaned up code and added more functions

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,49 +8,51 @@ namespace HelloDungeon
     {
         string playerName = "Empty";
         int playerHealth = 100;
-        //int level = 1;
+        int playerPower = 100;
         //float enemyHealth = 100.0f;
-        int playerAttackDamage1 = 30;
-        int playerAttackDamage2 = 20;
+        int playerAttackDamage = 30;
         //int enemyAttackDamage = 20;
-        int inventory = 0;
         int sword = 40;
         int gun = 40;
         string weapon = "Empty";
         bool playerDied;
-        string characterJob = "Empty";
+        string characterClass = "Empty";
 
+        //Display players stats
         void DisplayPlayerStats()
         {
             Console.WriteLine("Character Stats");
             Console.WriteLine("Name " + playerName);
+            Console.WriteLine("Class " + characterClass);
+            Console.WriteLine("Weapon " + weapon);
             Console.WriteLine("Health " + playerHealth);
-            Console.WriteLine("Main Attack Damage " + playerAttackDamage1);
-            Console.WriteLine("Secondary Attack Damage " + playerAttackDamage2);
+            Console.WriteLine("Power " + playerPower);
+            Console.WriteLine("Main Attack Damage " + playerAttackDamage);
         }
 
-        void CharacterJob()
+        /// <summary>
+        /// This function allows player to choose a class
+        /// </summary>
+        void CharacterClass()
         {
-            int input = GetInput("Pick a job!", "1.Wizard", "2.Knight");
-
-            
+            int input = GetInput("Pick a class!", "Warlock", "Elf");
 
             //If player chooses wizard
             if (input == 1)
             {
                 //Wizard Stats
-                characterJob = "Wizard";
-                playerHealth = 300;
-                playerAttackDamage1 = 400;
+                characterClass = "Warlock";
+                playerHealth = 100;
+                playerAttackDamage = 30;
                 
             }
             //If player chooses knight
             else if (input == 2)
             {
                 //Knight Stats
-                characterJob = "Knight";
-                playerHealth = 300;
-                playerAttackDamage2 = 400;
+                characterClass = "Elf";
+                playerHealth = 100;
+                playerAttackDamage = 30;
             }
             //If player chooses anything else
             else
@@ -58,6 +60,8 @@ namespace HelloDungeon
                 //Players input was invalid
                 Console.WriteLine("invalid input");
             }
+            Console.ReadKey();
+            Console.Clear();
 
         }
 
@@ -109,6 +113,85 @@ namespace HelloDungeon
             }
             Console.ReadKey();
             Console.Clear();
+        }
+
+        void Interaction2()
+        {
+            //declare number of players attempts
+            int numberOfAttempts = 4;
+            string input = " ";
+
+            Console.WriteLine("A very old man with a monkey on his back approaches you" +
+                "\n The monkey offers you immortality if you can solve a riddle in " + numberOfAttempts + " attempts");
+
+            Console.ReadKey();
+
+            //For Loop
+            for (int i = 0; i < numberOfAttempts; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("What has to be broken before you can use it?");
+                int attemptsRemaining = numberOfAttempts - i;
+                Console.WriteLine("Attempts Remaining: " + attemptsRemaining);
+                Console.Write("> ");
+                input = Console.ReadLine();
+
+                if (input == "egg")
+                {
+                    Console.WriteLine("Congrats! You have gained immortality");
+                    break;
+
+                }
+
+                Console.WriteLine("Incorrect! The monkey laughs at you. It hurts..." +
+                    "\n you take 5 points of damage.");
+                playerHealth -= 5;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        
+        /*
+        void FinalInteraction()
+        {
+            int input = GetInput("After picking a potion you continue on your adventure" +
+                "\n "
+        }
+        */
+        /// <summary>
+        /// Player stops at a merchant shop to choose if they want to replenish their health or power
+        /// </summary>
+        void MerchantStop()
+        {
+            playerHealth = 50;
+            playerPower = 50;
+
+            int input = GetInput("You have been traveling for about a week now and have had many incounters with enemys." +
+                "\n Due to these incounters you have lost 50 health and 50 power." +
+                "\n You have stopped at a merchant store to replenish but" +
+                "\n unfortantly you have a very limited amount of space and can only buy 1 item." +
+                "\n Items in stock: ", "1.Potion of Healing", "2. Potion of Power");
+
+            if (input == 1)
+            {
+                Console.WriteLine("You have choosen Potion of Healing");
+                playerHealth = 50 + playerHealth;
+               
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("You have choosen Poison.");
+                playerPower = 50 + playerHealth;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            
         }
 
 
@@ -167,35 +250,6 @@ namespace HelloDungeon
 
         public void Run()
         {
-            //
-            /*int healthRegen = 50;
-            int health = 100;
-            health += healthRegen;
-            Console.WriteLine(health);
-
-            string name = "Empty";
-            Console.WriteLine("Please enter your name. ");
-            name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-            */
-            //Test commit
-
-            /*string playerName = "Empty";
-            float playerHealth = 100.0f;
-            int level = 1;
-            float enemyHealth = 100.0f;
-            int playerAttackDamage1 = 30;
-            int playerAttackDamage2 = 20;
-            int enemyAttackDamage = 20;
-            int inventory = 0;
-            int sword = 40;
-            int gun = 40;
-            string weapon = "Empty";
-            bool playerDied;
-            string characterJob = "Empty";
-            */
-
-
             //Player enters there character name
             Console.WriteLine("What is your adventure name.");
             playerName = Console.ReadLine();
@@ -207,171 +261,26 @@ namespace HelloDungeon
             Console.ReadKey();
             Console.Clear();
 
-            //Get players input
-            //string input = GetInput();
-
-            //Display player weapon choice
-            //Console.WriteLine("Please choose starting weapon.");
-            //Console.WriteLine("1.Sword");
-            //Console.WriteLine("2.Gun");
 
             //Calling GetInput for player to choose a weapon
             GetInput("Weapon Choice", "Sword", "Gun");
 
+            //Calling function that lets player choose a class
+            CharacterClass();
+            //Calling function to display players stats after chooseing a class
             DisplayPlayerStats();
-            CharacterJob();
-            Console.ReadKey();
-            Console.Clear();
+          
+            //Player finds a dog interaction
+            Interaction1();
 
-             Interaction1();
+            //Player finds a man with a riddle interaction
+            Interaction2();
 
-        
+            //Player will choose between a healing potion and a power potion
+            MerchantStop();
 
-            //While loop exercise
-            //string characterJob = "Empty";
-            //string input = "Empty";
-            /*
-            //While loop condition declared
-            bool validInputReceived = false;
-            /* Note
-                !A && !B
-                !(A || B)
-            /*
-            //While input is not valid, displays choices again
-            while (validInputReceived == false)
-            {
-                //Pick a job excersie
-
-                //User job choices
-                Console.WriteLine("Pick a job");
-                Console.WriteLine("1.Wizard");
-                Console.WriteLine("2.Knight");
-                Console.Write(">");
-
-                //Gets player input
-                playerInput = Console.ReadLine();
-
-                //If player chooses wizard
-                if (playerInput == "1" || playerInput == "wizard")
-                {
-                    //Wizard Stats
-                    characterJob = "Wizard";
-                    playerHealth = 300;
-                    playerAttackDamage1 = 400;
-                    validInputReceived = true;
-                }
-                //If player chooses knight
-                else if (playerInput == "2" || playerInput == "knight")
-                {
-                    //Knight Stats
-                    characterJob = "Knight";
-                    playerHealth = 300;
-                    playerAttackDamage2 = 400;
-                    validInputReceived = true;
-                }
-                //If player chooses anything else
-                else
-                {
-                    //Players input was invalid
-                    Console.WriteLine("invalid input");
-                }
-                
-            }
-
-            Console.ReadKey();
-            Console.Clear();
-
-            
-            //Display players stats for player to see
-            Console.WriteLine("Your character stats");
-            Console.WriteLine("Character " + characterJob);
-            Console.WriteLine("Name " + playerName);
-            Console.WriteLine("Health " + playerHealth);
-            Console.WriteLine("Power " + playerAttackDamage1);
-            
-            //Stops console for player to read stats. Player can press any key to move on.
-            Console.ReadKey();
-            //Clears stats from console.
-            Console.Clear();
-
-            //First area player enters
-           
-
-            //For loop exercise
-
-            //declare number of players attempts
-            int numberOfAttempts = 4;
-
-            Console.WriteLine("A very old man with a monkey on his back approaches you" +
-                "\n The monkey offers you immortality if you can solve a riddle in " + numberOfAttempts + " attempts");
-
-            Console.ReadKey();
-
-            //For Loop
-            for(int i = 0; i < numberOfAttempts; i++)
-            {
-                Console.Clear();
-                Console.WriteLine("What has to be broken before you can use it?");
-                int attemptsRemaining = numberOfAttempts - i;
-                Console.WriteLine("Attempts Remaining: " + attemptsRemaining);
-                Console.Write("> ");
-                playerInput = Console.ReadLine();   
-
-                if (playerInput == "egg")
-                {
-                    Console.WriteLine("Congrats! You have gained immortality");
-                    break;
-
-                }
-
-                Console.WriteLine("Incorrect! The monkey laughs at you. It hurts..." +
-                    "\n you take 5 points of damage.");
-                playerHealth -= 5;
-            }
-
-            Console.ReadKey();
-            Console.Clear();
-
-            //If player has the dog, this event will happen
-            Console.WriteLine("You and your dog (name of dog, player will create, will be put here) have been traveling for about a week now" +
-                "and have had many incounters with enemys." + "\n You have now stopped at a merchant store to replenish your stock of potions." +
-                "\n You have a very limited amount of space and can only buy 2 items.");
-            Console.WriteLine("Items in store" +
-                "\n 1.Potion of healing +20 ($100)" + "\n 2.Poison" + "\n 3.Potion of Power");
-
-            bool validInputRecieved = true;
-            string playerInput = Console.ReadLine();
-
-            while (validInputRecieved == false) 
-            {
-                if (playerInput == "1" || playerInput == "Potion of healing")
-                {
-                    Console.WriteLine("You have choosen Potion of Healing");
-                    inventory = 1 + inventory;
-                }
-                else if (playerInput == "2" || playerInput == "Poison")
-                {
-                    Console.WriteLine("You have choosen Poison.");
-                    inventory = 1 + inventory;
-                }
-                else if (playerInput == "3" || playerInput == "Potion of Power") 
-                {
-                    Console.WriteLine("You have choosen Potion of Power.");
-                    inventory = 1 + inventory;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }*/
-
-
-
-             
-
-
-
-
+            //Displays player stats after choosing a potion
+            DisplayPlayerStats();
         }
     }
 }
